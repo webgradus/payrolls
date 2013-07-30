a1f554096cbe098f26bb7dd1d856429f29ba2492
GarPit <garik.piton@gmail.com>
2013-07-30 18:34:39 +0400

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :payrolls do
+  name 'Payrolls plugin'
+  author 'Gradus'
+  description 'Payrolls plugin for Redmine'
+  version '0.0.1'
+  url 'http://github.com/webgradus/payrolls'
+  author_url 'http://webgradus.ru'
+end
