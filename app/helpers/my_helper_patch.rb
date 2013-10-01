e96f3919e2cbb502fb69d7dfe413d0112e7c929e
GarPit <garik.piton@gmail.com>
2013-10-01 11:25:25 +0400

    add support for Trainee role and rate

--- app/helpers/my_helper_patch.rb
+++ app/helpers/my_helper_patch.rb
@@ -9,14 +9,20 @@ module MyHelperPatch
       entries_total_cost = 0
       grouped_entries = entries.group_by(&:issue)
       grouped_entries.each do |issue, entries|
+        project = issue.project
+        rate = if issue.assigned_to.roles_for_project(project).map(&:name).include?(t("trainee"))
+            Setting.plugin_payrolls['trainee_rate'].to_f
+        else
+            Setting.plugin_payrolls['developer_rate'].to_f
+        end
         all_issue_entries = TimeEntry.on_issue(issue).spent_between(Date.today - 6, Date.today).where("time_entries.created_on < ?", entries.first.created_on)
         estimated_hours = (issue.estimated_hours || 0) - (all_issue_entries - entries).sum(&:hours)
         user_issue_hours = entries.sum(&:hours)
         if user_issue_hours <= estimated_hours
-          entries_total_cost += user_issue_hours.to_f * Setting.plugin_payrolls['developer_rate'].to_f
+          entries_total_cost += user_issue_hours.to_f * rate
         else
-          entries_total_cost += estimated_hours.to_f * Setting.plugin_payrolls['developer_rate'].to_f
-          entries_total_cost += (estimated_hours - user_issue_hours).to_f * (Setting.plugin_payrolls['developer_rate'].to_f / 2)
+          entries_total_cost += estimated_hours.to_f * rate
+          entries_total_cost += (estimated_hours - user_issue_hours).to_f * (rate / 2)
         end
       end
       entries_total_cost
